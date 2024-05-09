a05a95bdd8e86ee43806dbc428449b0adc185fb1
knucklessg1 <knucklessg1@gmail.com>
2024-05-09 03:45:41 -0500

    Added pre-commit, reformatted all code.

--- report_manager/__init__.py
+++ report_manager/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 from report_manager.version import __version__, __author__, __credits__
 from report_manager.report_manager import ReportManager
 """
@@ -11,3 +12,5 @@ A simple way to manager your reports and analysis
 __version__ = __version__
 __author__ = __author__
 __credits__ = __credits__
+
+__all__ = ["ReportManager"]
